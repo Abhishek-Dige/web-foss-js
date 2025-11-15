3595b6f1008801efb305c786a45fb53be5854edd
Abhishek-Dige <abhishekdige22@gmail.com>
2025-11-15 19:18:34 +0530

    added a folder with my rollno

---
+++ CSB/LCB2025006/playground.js
@@ -0,0 +1,5 @@
+let button = document.querySelector(".change");
+button.addEventListener("click", function(){
+    let newelement = document.getElementById("heading");
+    newelement.style.color = "red";
+})
